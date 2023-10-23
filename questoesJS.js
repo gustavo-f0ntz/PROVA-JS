2b5dea8c6aefe4bc73110591c77685acbb080e40
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-23 20:48:09 -0300

    Initial commit

---
+++ questoesJS.js
@@ -0,0 +1,73 @@
+
+// QUESTÃO 1 - A
+
+function cadastrarTarefa() {
+   let tarefa = prompt("Cadastre uma tarefa: ");
+   console.log(tarefa);
+   window.alert(tarefa);
+    
+}
+
+cadastrarTarefa();
+
+// QUESTÃO 01 - B
+
+arrayDosNumeros = [1,2,3,4,5,6];
+arrayDosNomes = ["Gustavo", "Felipe", "Fabricio", "Abnoan", "Emanoel", "Davi"];
+
+function arrayNumbers() {
+    console.log(arrayDosNumeros);
+    
+
+}
+
+arrayNumbers();
+
+function arrayNomes() {
+    console.log(arrayDosNomes);
+}
+
+arrayNomes();
+
+function novoParamArray() {
+    paramdigitadoArray = prompt("Digite algum parametro com numero ou nome: ");
+
+}
+
+novoParamArray();
+
+// falta completar as questões 1- C  e as demais da 1.
+
+// colocar o verificador de arrays e outras coisas, continuo depois.
+
+
+
+
+
+// QUESTÃO 02
+
+const meusDados = {};
+meusDados.nome = prompt("Digite o nome:");
+meusDados.idade = prompt("Digite a idade:");
+meusDados.cidade = prompt("Digite a cidade:");
+meusDados.UF = prompt("Digite o UF:");
+meusDados.bairro = prompt("Digite o bairro:");
+console.log(meusDados);
+
+
+
+
+
+
+
+
+
+//QUESTÃO 1 - B
+
+
+
+// QUESTÃO 1 - C    
+
+
+
+
